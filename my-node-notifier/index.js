759abe1e08f1ff15403b549eb50fb5f3e2a08715
Andrey Serebrov <andser45@mail.ru>
2018-05-22 11:22:45 +0500

    make module

--- my-node-notifier/index.js
+++ my-node-notifier/index.js
@@ -1,42 +1,36 @@
-﻿const request = require('request');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const request = require('request');
 const Https = require('https');
 const HttpsProxyAgent = require('https-proxy-agent');
-
 class Notifier {
-    name: string;
-    sendMessage(message: string): void {
+    sendMessage(message) {
         console.log("Default implementation");
     }
 }
-
+exports.Notifier = Notifier;
 class TelegramNotifier extends Notifier {
-    token: string;
-    chatID: string;
-
-    init(name: string, token: string, chatID: string): void {
+    init(name, token, chatID) {
         this.name = name;
         this.token = token;
         this.chatID = chatID;
     }
-
-    sendMessage(message: string): void {
+    sendMessage(message) {
         if (message === undefined) {
             return;
         }
         message = this.bold("• " + this.name + " •") + '\n' + message;
         message = message.replace("+", "%2B");
-        let url: string = `https://api.telegram.org/bot${this.token}/sendMessage?chat_id=${this.chatID}&text=${message}&parse_mode=html`;
-
+        let url = `https://api.telegram.org/bot${this.token}/sendMessage?chat_id=${this.chatID}&text=${message}&parse_mode=html`;
         request(url, { json: true }, (err, res, body) => {
-            if (err) { return console.log(err); }
+            if (err) {
+                return console.log(err);
+            }
         });
-
-
         //var agent = new HttpsProxyAgent({
         //    proxyHost: '188.166.214.xxx',
         //    proxyPort: 1088
         //});
-
         //Https.request({
         //    // like you'd do it usually...
         //    host: 'api.telegram.org',
@@ -49,36 +43,33 @@ class TelegramNotifier extends Notifier {
         //        console.log(data.toString());
         //    });
         //}).end();
-
     }
-
-    bold(message: string): string {
+    bold(message) {
         if (message === undefined) {
             return;
         }
         return `<b>${message}</b>`;
     }
 }
-
+exports.TelegramNotifier = TelegramNotifier;
 class SmsNotifier extends Notifier {
-    token: string;
-    phone: string;
-
-    init(name: string, token: string, phone: string): void  {
+    init(name, token, phone) {
         this.name = name;
         this.token = token;
         this.phone = phone;
     }
-
-    sendMessage(message: string): void {
+    sendMessage(message) {
         if (message === undefined) {
             return;
         }
         message = message.replace("+", "%2B");
-        let url: string = `http://sms.ru/sms/send?api_id=${this.token}&to=${this.phone}&text=${message}`;
-
+        let url = `http://sms.ru/sms/send?api_id=${this.token}&to=${this.phone}&text=${message}`;
         request(url, { json: true }, (err, res, body) => {
-            if (err) { return console.log(err); }
+            if (err) {
+                return console.log(err);
+            }
         });
     }
 }
+exports.SmsNotifier = SmsNotifier;
+//# sourceMappingURL=index.js.map
---
+++ my-node-notifier/index.ts
@@ -0,0 +1,88 @@
+﻿const request = require('request');
+const Https = require('https');
+const HttpsProxyAgent = require('https-proxy-agent');
+
+class Notifier {
+    name: string;
+    sendMessage(message: string): void {
+        console.log("Default implementation");
+    }
+}
+
+class TelegramNotifier extends Notifier {
+    token: string;
+    chatID: string;
+
+    init(name: string, token: string, chatID: string): void {
+        this.name = name;
+        this.token = token;
+        this.chatID = chatID;
+    }
+
+    sendMessage(message: string): void {
+        if (message === undefined) {
+            return;
+        }
+        message = this.bold("• " + this.name + " •") + '\n' + message;
+        message = message.replace("+", "%2B");
+        let url: string = `https://api.telegram.org/bot${this.token}/sendMessage?chat_id=${this.chatID}&text=${message}&parse_mode=html`;
+
+        request(url, { json: true }, (err, res, body) => {
+            if (err) { return console.log(err); }
+        });
+
+
+        //var agent = new HttpsProxyAgent({
+        //    proxyHost: '188.166.214.xxx',
+        //    proxyPort: 1088
+        //});
+
+        //Https.request({
+        //    // like you'd do it usually...
+        //    host: 'api.telegram.org',
+        //    port: 443,
+        //    method: 'GET',
+        //    path: '/',
+        //    agent: agent
+        //}, function (res) {
+        //    res.on('data', function (data) {
+        //        console.log(data.toString());
+        //    });
+        //}).end();
+
+    }
+
+    bold(message: string): string {
+        if (message === undefined) {
+            return;
+        }
+        return `<b>${message}</b>`;
+    }
+}
+
+class SmsNotifier extends Notifier {
+    token: string;
+    phone: string;
+
+    init(name: string, token: string, phone: string): void  {
+        this.name = name;
+        this.token = token;
+        this.phone = phone;
+    }
+
+    sendMessage(message: string): void {
+        if (message === undefined) {
+            return;
+        }
+        message = message.replace("+", "%2B");
+        let url: string = `http://sms.ru/sms/send?api_id=${this.token}&to=${this.phone}&text=${message}`;
+
+        request(url, { json: true }, (err, res, body) => {
+            if (err) { return console.log(err); }
+        });
+    }
+}
+
+export { TelegramNotifier };
+export { SmsNotifier };
+export { Notifier };
